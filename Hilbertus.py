f0ec1f82b9e6ed8c9998c7eb95e339518493c5b6
Eliška Niemczyková <nie0087@vsb.cz>
2025-08-05 14:41:45 +0200

    corrected 3D HIilberts curve and linear interpolation for 2D Hilbert added

--- Hilbertus.py
+++ Hilbertus.py
@@ -1,7 +1,8 @@
 import math
 import numpy as np
 from scipy.optimize import minimize_scalar
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class Hilbert2D:
 
@@ -9,32 +10,22 @@ class Hilbert2D:
         self.precision = precision
 
     # --- Konverze ---
-    def dec_to_octal(self, number: float):
-        q_num = []
-        i = 0
-        if 0 < number < 1:
-            while number != 0 and i < self.precision:
-                number *= 8
-                digit = math.floor(number)
-                q_num.append(digit)
-                number -= digit
-                i += 1
-        return q_num
-
     def dec_to_quarter(self, number: float):
         q_num = []
         i = 0
-        if 0 < number < 1:
+        if 0 < number <= 1:
             while number != 0 and i < self.precision:
                 number *= 4
                 digit = math.floor(number)
                 q_num.append(digit)
                 number -= digit
                 i += 1
-        elif number == 1:
-            q_num.append(1)
         elif number == 0:
             q_num.append(0)
+        
+        elif number == 1.0:
+            q_num.append(1)
+        
         return q_num
 
     # --- Hilbert 2D ---
@@ -65,35 +56,72 @@ class Hilbert2D:
         return s
 
     def hilbert_point(self, t):
+        if t == 1.0:
+            return np.array([1.0, 0.0]) 
+        
         q = self.dec_to_quarter(t)
         e0, dj = self.ej_and_dj_counter(q)
         point = self.calculate_point(e0, dj, q)
         return point.flatten()
+    
+    def hilbert_polygon_point(self, t, n):
+    
+        N = 2**(2*n)
+    
+    
+        k = int(np.floor(t * N))
+        #if t == 1.0: chaby pokus 
+        #   t = 1.0 - 1e-12
+
+        p_k = self.hilbert_point(k / N)
+        p_k1 = self.hilbert_point((k + 1) / N)
+        
+        point = N * (t-(k/N))*p_k1 - N*(t-((k+1)/N))*p_k
+
+        return point
+    
+    def plot_hilbert_polygon(self, n):
+        
+
+        N = 2**(2*n)
+        points = []
+
+        for k in range(N+1):
+            t = k / N
+            points.append(self.hilbert_polygon_point(t,n))
+
+        points = np.array(points)
+        plt.plot(points[:,0], points[:,1], '-o', markersize=2)
+        plt.axis('equal')
+        plt.show()
+
+
+
 
     # --- Optimalizace ---
     @staticmethod
     def f(x, y):
-        return (x - 0.5)**2 + (y - 0.5)**2
+        return ((x - 0.3)**2 + (y - 0.7)**2)**1/2 + 1
 
-    def F(self, t):
-        x, y = self.hilbert_point(t)
+    def F(self, t, n):
+        x, y = self.hilbert_polygon_point(t,n)
         return self.f(x, y)
    
     # --- Zabudovana python metoda pro hledani minima ---
    
-    def find_minimum(self):
-        result = minimize_scalar(lambda t: self.F(t), bounds=(0, 1), method='bounded')
+    def find_minimum(self,n):
+        result = minimize_scalar(lambda t: self.F(t,n), bounds=(0, 1), method='bounded')
         t_min = result.x
         h_min = self.hilbert_point(t_min)
         f_min = self.f(*h_min)
         return t_min, h_min, f_min
    
     # --- Optimalizacni algoritmus pro hledani minima ---
-    def Holder_algorithm(self,H,r,eps,max_iter):
+    def Holder_algorithm(self,H,r,eps,max_iter,n):
         N = 2                      
         # STEP 0: inicializace
         xk = [0.0, 1.0]
-        zk = [self.F(0.0), self.F(1.0)]
+        zk = [self.F(0.0,n), self.F(1.0,n)]
         k = 2
 
         for iteracni_krok in range(max_iter):
@@ -104,9 +132,9 @@ class Hilbert2D:
 
             # STEP 2: odhad Holderovy konstanty
             hvalues = []
-            #for i in range(1, len(xk)):
-            #    diff = abs(zk[i] - zk[i-1]) / (abs(xk[i] - xk[i-1]))**(1/N) if abs(xk[i]-xk[i-1])>0 else 0
-            #    hvalues.append(diff)
+            for i in range(1, len(xk)):
+               diff = abs(zk[i] - zk[i-1]) / (abs(xk[i] - xk[i-1]))**(1/N) if abs(xk[i]-xk[i-1])>0 else 0
+               hvalues.append(diff)
             h_hat = max(hvalues) if hvalues else H
             h_used = max([h_hat, 1e-8])   
     
@@ -131,7 +159,7 @@ class Hilbert2D:
 
            
             xk.append(y_star)
-            zk.append(self.F(y_star))
+            zk.append(self.F(y_star,n))
             k += 1
         
         min_idx = np.argmin(zk)
@@ -143,10 +171,34 @@ class Hilbert2D:
 
 
 class Hilbert3D:
+    
+    def __init__(self, precision: int):
+        self.precision = precision
+
+    def dec_to_octal(self, number: float):
+        q_num = []
+        i = 0
+
+        if 0 < number < 1:
+            while number != 0 and i < self.precision:
+                number *= 8
+                digit = math.floor(number)
+                q_num.append(digit)
+                number -= digit
+                i += 1
+        elif number == 0:
+            q_num.append(0)
+        
+        elif number == 1.0:
+            q_num.append(1)
+
+        return q_num
+    
+    
     def ThreeD_Hilbert(self, q_num):
         H_all = [
             np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]]),
-            np.array([[1, 0, 0], [0, 1, 0], [1, 0, 0]]),
+            np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]]),
             np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
             np.array([[0, 0, 1], [-1, 0, 0], [0, -1, 0]]),
             np.array([[0, 0, -1], [-1, 0, 0], [0, 1, 0]]),
@@ -157,7 +209,7 @@ class Hilbert3D:
 
         h_all = [
             np.array([0, 0, 0]),
-            np.array([0, 1, 1]),
+            np.array([0, 1, 0]),
             np.array([1, 1, 0]),
             np.array([1, 1, 1]),
             np.array([2, 1, 1]),
@@ -176,3 +228,26 @@ class Hilbert3D:
             soucin = np.eye(3)
 
         return s
+    
+    def hilbert3D_point(self, t):
+
+        q = self.dec_to_octal(t)
+        point = self.ThreeD_Hilbert(q)
+        return point
+
+
+    def plot_3d_hilbert(self, order=2, num_points=2000):
+        t_values = np.linspace(0, 1, num_points)
+        points = []
+        for t in t_values:
+            pt = self.hilbert3D_point(t)
+            points.append(pt)
+        points = np.array(points)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(points[:,0], points[:,1], points[:,2], lw=1)
+        ax.set_title(f'3D Hilbert Curve (order={order})')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.show()
